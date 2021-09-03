ad99740b49547f5756608b971114b410207ec539
Anaya Gandhi <76689936+AnayaGandhi@users.noreply.github.com>
2021-09-03 16:44:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,21 +13,32 @@ recognition.onresult = function(event) {
     console.log(event);
 
     var Content = event.resutls[0][0].transcript;
-    console.log(Content);
 
     document.getElementById("textbox").innerHTML = Content;
-    speak();
+    console.log(Content);
+
+    if(Content =="take my sefie")
+    {
+        console.log("taking selfie --- ");
+        speak();
+    }
 }
 
 function speak(){
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value
+    speak_data = "Taking your Selfie in 5 seconds";
 
     var utterThis= new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    }, 5000);
 }
 
 Webcam.set({
@@ -37,3 +48,17 @@ Webcam.set({
     png_quality:90
 });
 camera= document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.click();
+}
